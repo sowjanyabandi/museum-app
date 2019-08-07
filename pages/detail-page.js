dcd3d9ed64d71a2c59be210831d46e7b51bf043b
sowjanya ayanampudi <sowjanyabandi@gmail.com>
2019-08-07 12:15:43 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -9,12 +9,16 @@ function submitComment()
    const h3 = document.createElement('h3'); 
    const p = document.createElement('p');
    
-   h3.innerHTML = '${name} said:'
+   h3.innerHTML = `${name} said:`
    p.innerHTML = msg
    comment.classList.add('comment')
    comment.appendChild(h3)
    comment.appendChild(p)
-
-   console.log(comment)
+   const commentSection = document.getElementById('comments')
+   //console.log(commentSection)
+   //console.log(comment)
+   commentSection.appendChild(comment)
+inputField.value = null;
+textArea.value = null;
 
 }
